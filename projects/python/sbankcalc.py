058e5a7901f3f3c558b6dc7bcd156477dd615161
Victor Varenik <38311102+kotleni@users.noreply.github.com>
2021-08-22 20:06:31 +0300

    Delete sbankcalc.py

--- projects/python/sbankcalc.py
+++
@@ -1,5 +0,0 @@
-def calcsum(count):
-    return (count * 670) * (30 / 7)
-
-rub = int(calcsum(int(input("Сколько раз за неделю вы онанируете?: "))))
-print("Вы бы могли заработать на этом: " + str(rub) + " рублей")
