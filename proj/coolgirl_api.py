bebfadbf2c997b2bd2ea11aa008456f2670a73b2
Victor Varenik <38311102+kotleni@users.noreply.github.com>
2021-10-11 11:43:36 +0300

    Create coolgirl_api.py

---
+++ proj/coolgirl_api.py
@@ -0,0 +1,18 @@
+# Cluster Coolgirl API
+# https://coolgirl.clusterrr.com/v1.2/{method}
+
+# /games                         - Get games list
+# /roms/defaults?hash={hash}     - Reset roms change to default
+# /clear-cache                   - Clear cache          (ClUSTER_ONLY)
+# /roms/{rom_id}/description     - Set rom description  (ClUSTER_ONLY) 
+# /games/{game_id}/description   - Set game description (ClUSTER_ONLY) 
+# /config?hash={hash}            - Get config for rom compiler
+# /is_admin                      - Check, is you admin
+# /games/game_categories/        - Get games categories
+# /games/hack_categories/        - Get games hacks categories
+# /mappers                       - Get all mappers
+# /preview?hash={hash}           - Get rom project info
+# /compile?hash={hash}           - Try compile          (NOT_WORKING)
+
+# download game screenshot
+# https://coolgirl.clusterrr.com/screenshots/{rom_file}_{img_id}.png
