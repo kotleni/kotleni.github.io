cdb2b45a40d02312bd270eb978c20079974fb171
kotleni <yavarenikya@gmail.com>
2022-09-30 22:37:23 +0300

    xx

---
+++ projects/js/ReallySuperGame/src/level.js
@@ -0,0 +1,173 @@
+import * as THREE from './three.module.js';
+
+const BLOCK_SCALE = 0.5
+const LEVEL1 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+const LEVEL2 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+const LEVEL3 = [
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+const LEVEL = [LEVEL1, LEVEL2, LEVEL3]
+
+const textureBricks = new THREE.TextureLoader().load("bricks.jpeg")
+textureBricks.wrapS = THREE.RepeatWrapping
+textureBricks.wrapT = THREE.RepeatWrapping
+textureBricks.repeat.set(2, 2)
+
+const textureBricks2 = new THREE.TextureLoader().load("bricks.jpeg")
+textureBricks2.wrapS = THREE.RepeatWrapping
+textureBricks2.wrapT = THREE.RepeatWrapping
+textureBricks2.repeat.set(5, 2)
+
+const textureBricks3 = new THREE.TextureLoader().load("brick2.jpeg")
+textureBricks3.wrapS = THREE.RepeatWrapping
+textureBricks3.wrapT = THREE.RepeatWrapping
+textureBricks3.repeat.set(2, 2)
+
+const textureGround = new THREE.TextureLoader().load("ground.png")
+textureGround.wrapS = THREE.RepeatWrapping
+textureGround.wrapT = THREE.RepeatWrapping
+textureGround.repeat.set(50, 50)
+
+const textures = {
+    bricks: textureBricks,
+    bricks2: textureBricks2,
+    bricks3: textureBricks3,
+    ground: textureGround
+}
+
+function generateLevel(scene) {
+    // sun light
+    // const directionalLight = new THREE.DirectionalLight(0x000000, 1)
+    // directionalLight.position.set(0, BLOCK_SCALE* (LEVEL.length + 4), 0)
+    // directionalLight.castShadow = true
+    // scene.add(directionalLight)
+
+    const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+	light.position.set(0.5, 1, 0.75);
+	scene.add(light);
+
+    // ground
+    const groundgeom = new THREE.BoxGeometry(BLOCK_SCALE * (LEVEL[0].length * 2), 0.1, BLOCK_SCALE * (LEVEL[0][0].length * 2))
+    const groundmat = new THREE.MeshPhongMaterial({ map: textures.ground })
+    const cubet = new THREE.Mesh(groundgeom, groundmat)
+    cubet.position.y = -0.1
+    cubet.receiveShadow = true
+    scene.add(cubet)
+
+    for(let y = 0; y < LEVEL.length; y += 1) {
+        for(let x = 0; x < LEVEL[y].length; x += 1) {
+            for(let z = 0; z < LEVEL[y][x].length; z += 1) {
+                let item = LEVEL[y][x][z]
+
+                if(item == 1) {
+                    const obj = new THREE.Mesh(
+                        new THREE.BoxGeometry(BLOCK_SCALE, BLOCK_SCALE, BLOCK_SCALE), 
+                        new THREE.MeshPhongMaterial({ map: textures.bricks})
+                    );
+                    obj.receiveShadow = true
+                    obj.castShadow = true
+                    scene.add(obj)
+
+                    obj.position.set(
+                        x * BLOCK_SCALE,
+                        y * BLOCK_SCALE,
+                        z * BLOCK_SCALE
+                    )
+                } else if(item == 2) {
+                    const obj = new THREE.Mesh(
+                        new THREE.CylinderGeometry(BLOCK_SCALE/2, BLOCK_SCALE/2, BLOCK_SCALE, 40), 
+                        new THREE.MeshPhongMaterial({ map: textures.bricks2 })
+                    );
+                    obj.receiveShadow = true
+                    obj.castShadow = true
+                    scene.add(obj)
+
+                    obj.position.set(
+                        x * BLOCK_SCALE,
+                        y * BLOCK_SCALE,
+                        z * BLOCK_SCALE
+                    )
+                } else if(item == 3) {
+                    const obj = new THREE.Mesh(
+                        new THREE.BoxGeometry(BLOCK_SCALE, BLOCK_SCALE / 6, BLOCK_SCALE), 
+                        new THREE.MeshPhongMaterial({ map: textures.bricks3 })
+                    );
+                    obj.receiveShadow = true
+                    obj.castShadow = true
+                    scene.add(obj)
+
+                    obj.position.set(
+                        x * BLOCK_SCALE,
+                        (y * BLOCK_SCALE) - BLOCK_SCALE + (BLOCK_SCALE - (BLOCK_SCALE / 2)),
+                        z * BLOCK_SCALE
+                    )
+                }
+            }
+        }
+    }
+}
+
+export { generateLevel }
---
+++ projects/js/ReallySuperGame/src/utils.js
@@ -0,0 +1,6 @@
+function degrees_to_radians(degrees) {
+    let pi = Math.PI
+    return degrees * (pi/180)
+}
+
+export { degrees_to_radians }
