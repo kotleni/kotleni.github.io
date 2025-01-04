187bf603bc7426dc836902b8fd7ed351241571f7
Viktor Varenik <yavarenikya@gmail.com>
2025-01-04 02:32:46 +0200

    chore(refactor): reimpl ParallaxBackground in functional way

--- src/Components/ParallaxBackground.tsx
+++ src/Components/ParallaxBackground.tsx
@@ -1,56 +1,42 @@
-import { Component } from 'react';
+import {useEffect, useState} from 'react';
 import '../App.scss';
 import editor_image from './editor.png'
 
-class ParallaxBackground extends Component<object, {position: { x: number; y: number; }}> {
-  sensitivity = 1.4;
+const SENSITIVITY = 1.4;
 
-  constructor() {
-    super({});
-    this.state = {
-      position: { x: 0, y: 0 },
-    };
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('mousemove', this.handleMouseMove);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('mousemove', this.handleMouseMove);
-  }
-
-  handleMouseMove(e: MouseEvent) {
+function ParallaxBackground() {
+  const [{x, y}, setPosition] = useState({x: 0, y: 0});
+  const handleMouseMove = (e: MouseEvent) => {
     // Get the mouse coordinates
     const mouseX = e.pageX;
     const mouseY = e.pageY;
 
     // Calculate the new position for the parallax effect
-    const newX = (mouseX / window.innerWidth) * this.sensitivity;
-    const newY = (mouseY / window.innerHeight) * this.sensitivity;
+    const newX = (mouseX / window.innerWidth) * SENSITIVITY;
+    const newY = (mouseY / window.innerHeight) * SENSITIVITY;
 
     // Update the position state
-    this.setState({
-      position: { x: newX, y: newY },
-    });
+    setPosition({ x: newX, y: newY });
   }
 
-  render() {
-    const { x, y } = this.state.position;
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }; // as didUnmount
+  }, [] /* as didMount */)
 
-    return (
+  return (
       <div
-        className="parallax"
-        style={{
-          opacity: 0.9,
-          transform: `translate(${x}%, ${y}%)`,
-        }}
+          className="parallax"
+          style={{
+            transform: `translate(${x}%, ${y}%)`,
+          }}
       >
-        <div className="parallax-container"><img className="parallax-image" src={editor_image} alt="Code Editor"></img></div>
+        <div className="parallax-container"><img className="parallax-image" src={editor_image} alt="Code Editor"></img>
+        </div>
       </div>
-    );
-  }
+  );
 }
 
 export default ParallaxBackground;
