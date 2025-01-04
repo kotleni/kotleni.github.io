1af6e3affc8383413e632c0f78ae2530c6780c48
Viktor Varenik <yavarenikya@gmail.com>
2025-01-04 14:11:05 +0200

    feat: upd version tip logic, add ester egg clicker

--- src/components/VersionTip/index.tsx
+++ src/components/VersionTip/index.tsx
@@ -1,10 +1,20 @@
-import {VERSION_NAME} from "../../constants.ts";
+import {VERSION_NAME, VERSION_NAME_REPLACEMENT} from "../../constants.ts";
 
 import './VersionTip.scss';
+import {useState} from "react";
 
 function VersionTip() {
+    const [clicksCount, setClicksCount] = useState(0);
+
+    const increaseClicks = () => {
+        setClicksCount(clicksCount + 1);
+    };
     return (
-        <div className="version-tip">{VERSION_NAME}</div>
+        <div className="version-tip" onClick={increaseClicks}>{
+            clicksCount < 8 ?
+                (clicksCount % 2 === 0 ? VERSION_NAME : VERSION_NAME_REPLACEMENT)
+                : `Clicks count: ${clicksCount}`
+        }</div>
     );
 }
 
