96be0930fe05351ec9a9028715980e06ff18df1a
Viktor Varenik <yavarenikya@gmail.com>
2025-01-26 23:53:01 +0200

    feat: reimplemented basic page design

---
+++ src/components/DottedBackground/index.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from "react";
+import Perlin from "../../perlin";
+import "./DottedBackground.scss";
+
+const SCALE = 200;
+const LENGTH = 10;
+const SPACING = 15;
+
+export default function CoolBackgroundCanvas() {
+    const [noise] = useState(new Perlin(Math.random()));
+
+    function getForceOnPoint(x: number, y: number, z: number) {
+        return (noise.perlin3(x / SCALE, y / SCALE, z) - 0.5) * 2 * (Math.PI * 2)
+    }
+
+    const [existingPoints, setExistingPoints] = useState(new Set<string>);
+    const [points, setPoints] = useState<{ x: number, y: number, opacity: number }[]>([]);
+
+    function addPoints(w: number, h: number) {
+        const points2: { x: number, y: number, opacity: number }[] = [];
+
+        for (let x = -SPACING / 2; x < w + SPACING; x += SPACING) {
+            for (let y = -SPACING / 2; y < h + SPACING; y += SPACING) {
+                const id = `${x}-${y}`
+                if (existingPoints.has(id))
+                    continue
+                setExistingPoints(existingPoints.add(id));
+
+                points2.push({ x, y, opacity: Math.random() * 0.5 + 0.5 })
+            }
+        }
+
+        setPoints(points2);
+    }
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const frameRef = useRef<number>(0);
+
+    useEffect(() => {
+        function draw(context: CanvasRenderingContext2D) {
+            if (context) {
+                context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+                const t = +new Date() / 10000
+
+                for (const p of points) {
+                    const radius = 1;
+                    const {x, y} = p;
+                    const rad = getForceOnPoint(x, y, t);
+                    const length = (noise.perlin3(x / SCALE, y / SCALE, t * 2) + 0.5) * LENGTH;
+                    const nx = x + Math.cos(rad) * length;
+                    const ny = y + Math.sin(rad) * length;
+
+                    const alpha = ((Math.abs(Math.cos(rad)) * 0.8 + 0.2) * p.opacity);
+                    context.fillStyle = `rgba(200, 200, 200, ${alpha})`;
+
+                    context.beginPath();
+                    context.arc(
+                        nx,
+                        ny,
+                        radius,
+                        0,
+                        Math.PI * 2
+                    );
+                    context.fill();
+                }
+
+                // Continue animation
+                frameRef.current = requestAnimationFrame(() => draw(context));
+            }
+        }
+
+        if (canvasRef.current) {
+            const context = canvasRef.current.getContext("2d");
+
+            if (context) {
+                const parent = context.canvas.parentElement!;
+                context.canvas.width = parent.offsetWidth;
+                context.canvas.height = parent.offsetHeight;
+
+                if(points.length <= 0)
+                    addPoints(context.canvas.width, context.canvas.height);
+
+                console.log("WWWWW")
+
+                frameRef.current = requestAnimationFrame(() => draw(context));
+            }
+        }
+
+        return () => cancelAnimationFrame(frameRef.current);
+    }, [noise, points, existingPoints]);
+
+    return <canvas className="canvas-dotted-background" ref={canvasRef} />;
+}
--- src/routes/App/index.tsx
+++ src/routes/App/index.tsx
@@ -1,32 +1,92 @@
-import {Link} from "react-router";
-import VersionTip from "../../components/VersionTip";
-import SocialItem from "../../components/SocialItem";
-import ParallaxBackground from "../../components/ParallaxBackground";
-
 import './App.scss';
+import { MdArrowOutward } from "react-icons/md";
+// import { MdLightMode } from "react-icons/md";
+import CoolBackgroundCanvas from "../../components/DottedBackground";
+import PizzaIcon from "../../assets/pizza.svg?react";
+
+type BadgeInfo = { title: string, link: string };
+
+const FULL_NAME = "Viktor Varenik";
+
+function Badge(props: { badge: BadgeInfo }) {
+    return <a href={props.badge.link} className="badge">{props.badge.title}</a>;
+}
 
 export default function App() {
+    const workingAt: BadgeInfo = { title: "Nowhere", link: "#" };
+    const creatorOf: BadgeInfo[] = [
+        { title: "Ukraine Metro App", link: "https://github.com/UniconTeam/UkraineMetro-android" },
+        { title: "iPhoneVR", link: "https://github.com/kotleni/iPhoneVR" },
+        { title: "MeowTG", link: "https://github.com/kotleni/meowtg" },
+    ];
+    const inTeams: BadgeInfo[] = [
+        { title: "Unicon Team", link: "https://github.com/UniconTeam" },
+        { title: "TimeMates", link: "https://github.com/timemates" },
+    ];
+    const findMeOn: BadgeInfo[] = [
+        { title: "GitHub", link: "https://github.com/kotleni" },
+        { title: "LinkedIn", link: "https://www.linkedin.com/in/kotleni/" },
+        { title: "Twitter", link: "https://x.com/kotleni_" },
+        { title: "Telegram", link: "https://t.me/kotleni" },
+    ];
+
     return (
         <>
-            <ParallaxBackground/>
+            <div className="top-bar">
+                <div className="logo">
+                    <PizzaIcon />
+                </div>
+                <a className="bar-link" href="/">Home</a>
+                <a className="bar-link" href="/">Contacts</a>
+                <a className="bar-link" href="/">Source code <MdArrowOutward/></a>
+                {/*<a className="bar-link" href="/"><MdLightMode/></a>*/}
+            </div>
 
-            <header className="header">
-                <Link className="header-title" to="/rick">Viktor Varenik</Link>
-                <h2 className="header-subtitle">Fullstack Web & Mobile developer</h2>
-            </header>
+            <div className="showcases-container">
+                <div className="showcase-1">
+                    <p className="text-title">{FULL_NAME}</p>
+                    <p className="text-description">Hey, I am {FULL_NAME}, a fanatical open sourceror.</p>
 
-            <footer className="footer">
-                <p className="footer-desc">I'm a developer based in Kharkiv, Ukraine. Specialized in developing fullstack and mobile applications using different technologies.</p>
-                <ul className="social">
-                    <h3 className="social-title">Contacts:</h3>
-                    <SocialItem title="email" link="mailto:yavarenikya@gmail.com"/>
-                    <SocialItem title="telegram" link="https://t.me/kotleni"/>
-                    <SocialItem title="github" link="https://github.com/kotleni"/>
-                    <SocialItem title="linkedin" link="https://www.linkedin.com/in/kotleni"/>
-                </ul>
-            </footer>
+                    <p className="text-description">
+                        <span className="text-overline">Working at&nbsp; <Badge badge={workingAt} /></span>
+                        <br/>
+                        Creator of&nbsp;
+                        {creatorOf.map((creatorOfIt) => {
+                            return <Badge badge={creatorOfIt}/>;
+                        })}
+                        <br/>
+                        Core team of&nbsp;
+                        {inTeams.map((it) => {
+                            return <Badge badge={it}/>;
+                        })}
+                    </p>
 
-            <VersionTip/>
+                    <p className="text-description">Dreaming up ideas and making them come true is where my passion
+                        lies. You can find my full projects list here. I also do some things with Linux and
+                        embedded.</p>
+                    <p className="text-description">I give help and write blog posts about open source, coding,
+                        tutorials, etc.</p>
+                    <p className="text-description">Outside of programming, I enjoy doing photography and engineering.
+                        Right now I live in Ukraine, Kremenchuk. If you are around, feel free to reach me out, we could
+                        have some coffee or work together.</p>
+
+                    <p className="text-description">
+                        Find me on<br/>
+                        {findMeOn.map((contribution) => {
+                            return <a href={contribution.link} className="badge">{contribution.title}</a>;
+                        })}
+                        <br/>
+                        Or mail me at <span className="email">yavarenikya@gmail.com</span>
+                    </p>
+
+                    {/*<p className="text-description">If you enjoy my work and find them useful, consider sponsor me and*/}
+                    {/*    the ecosystem to help Open Source sustainable. Thank you! </p>*/}
+                </div>
+            </div>
+
+            <div className="background">
+                <CoolBackgroundCanvas/>
+            </div>
         </>
     );
 }
