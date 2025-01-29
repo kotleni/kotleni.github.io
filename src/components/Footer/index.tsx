accf5607dec0f2bf07397ddc37eb6a229845d98d
Viktor Varenik <yavarenikya@gmail.com>
2025-01-30 00:14:54 +0200

    chore: split footer styles to separated module css
    Signed-off-by: Viktor Varenik <yavarenikya@gmail.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,23 @@
 import {NavSocialLink} from "../NavbarMenu/index.js";
+import styles from "./Footer.module.scss";
 
 type Props = { socialLinks: NavSocialLink[] };
 
 const Footer: React.FC<Props> = (props) => {
     return (
-        <footer className="footer">
-            <section className="footer-section">
-                <nav className="footer-menu">
+        <footer className={styles.footer}>
+            <section className={styles.section}>
+                <nav className={styles.menu}>
                     {props.socialLinks.map((link) => (
-                        <a href={link.url} target="_blank" rel="noopener noreferrer" className="footer-link">
-                            <div className="footer-link-icon">
+                        <a href={link.url} target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
+                            <div>
                                 {link.icon({})}
                             </div>
                             {link.title}
                         </a>
                     ))}
                 </nav>
-                <div className="footer-made-by">Made with ❤️ by Viktor Varenik</div>
+                <div className={styles.madeBy}>Made with ❤️ by Viktor Varenik</div>
             </section>
         </footer>
     );
