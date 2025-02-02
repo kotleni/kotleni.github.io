57913820b882ec8e7b6b6b307a52ddeb4a7193c0
Viktor Varenik <yavarenikya@gmail.com>
2025-02-02 18:58:47 +0000

    fix: wrong url's for deploying on gh-pages (#10)

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,21 @@ import {AboutPage} from "./routes/App/AboutPage";
 import {ProjectsPage} from "./routes/App/ProjectsPage";
 import {ArticlesPage} from "./routes/App/ArticlesPage";
 
+// Prefix is needed for github pages deploying
+const URL_PREFIX = "/me-react";
+
+function url(path: string) {
+    return `${URL_PREFIX}/${path}`;
+}
+
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App><HomePage/></App>} />
-            <Route path="/projects" element={<App><ProjectsPage/></App>} />
-            <Route path="/me" element={<App><AboutPage/></App>} />
-            <Route path="/articles" element={<App><ArticlesPage /></App>} />
-            <Route path="/rick" element={<Rick />} />
+            <Route path={url("")} element={<App><HomePage/></App>} />
+            <Route path={url("projects")} element={<App><ProjectsPage/></App>} />
+            <Route path={url("me")} element={<App><AboutPage/></App>} />
+            <Route path={url("articles")} element={<App><ArticlesPage/></App>} />
+            <Route path={url("rick")} element={<Rick />} />
         </Routes>
     </BrowserRouter>
 )
