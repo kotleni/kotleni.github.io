98b898d9e75a03c110d162a5159c6653414c7a29
Viktor Varenik <yavarenikya@gmail.com>
2025-02-02 19:06:51 +0200

    feat: rewrite about page

--- src/routes/App/AboutPage/index.tsx
+++ src/routes/App/AboutPage/index.tsx
@@ -1,15 +1,25 @@
 import Photo1 from "../../../assets/photo1.jpeg";
+import {SOCIAL_LINKS} from "../../../config.ts";
 
 const AboutPage: React.FC = () => {
     return (
         <>
             <header className="hero hero-page"><h1>About Me</h1></header>
-            <p>Hey, I'm Viktor! I XXXX, and fill me please. This text is useless, i just need to write it later. It's
-                just a stub, and you are so kind if you're reading this.</p>
-            <p>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
-                blah blah blah blah blah blah blah blah blah blah blah. Blah blah.</p>
-            <p>My site has <strong>no ads, no tracking or analytics, no sponsored posts and no paywall.</strong> Bla
-                blah blah. My motivation is blah blah blah blah. BLah blah!</p>
+            <p>
+                I'm a software engineer, open-source enthusiast, and retro tech lover.
+                <strong> I started my career as an Android developer</strong>, spending
+                three years crafting mobile experiences <strong>before shifting my focus to web
+                development in 2024</strong>. I've been started writing on this blog for the past
+                decade, sharing insights on coding, open-source projects, and my passion for all
+                things retro. Also, by the way — <i>I use Arch</i>, btw.
+            </p>
+            <p>
+                Also i enjoy languages, learning new things, coding, and making cool stuff for Linux.
+                When I'm not playing retro games or tweaking my Linux setup, I'm contributing to
+                open-source projects or experimenting with new tech.
+            </p>
+            <p>My site has <strong>no ads, no tracking or analytics, no sponsored posts, and no paywall</strong>.
+                It's a place for genuine content, driven by passion rather than profit.</p>
             <p>You can also follow me on Twitter or Github, blah blah. Also if you're interested in communication or
                 blah blah you can blah blah blah <a href="#">blah</a> blah.</p>
             <img src={Photo1} alt="Photo taken from bridge"/>
@@ -17,29 +27,22 @@ const AboutPage: React.FC = () => {
             <h2>Contact</h2>
             <p>Send me an email to say hi, or connect via the socials.</p>
             <ul>
-                <li>Blah 0!</li>
-                <li>Blah 1!</li>
-                <li>Blah 2!</li>
-                <li>Blah 3!</li>
-            </ul>
-
-            <h2>What I'm Doing Now</h2>
-            <p>
-                <em>Updated January Xst, 2025</em>
-            </p>
-            <ul>
-                <li>Working full time</li>
-                <li>Building new website for the sixth time</li>
-                <li>Learning something new</li>
-                <li>Watching <b>Mr. Robot</b> tv series</li>
-                <li>Doing blah blah blah</li>
+                {SOCIAL_LINKS.map((social) => (
+                    <li><a href={social.url}>{social.title}</a></li>
+                ))}
             </ul>
 
             <h2>Tools</h2>
             <h3>Software</h3>
             <p>This website is hosted on X and uses X.</p>
             <ul>
-                <li><strong>OS: </strong>Arch Linux with KDE</li>
+                <li><strong>OS: </strong>
+                    <ul>
+                        <li>Desktop: <a href="https://archlinux.org/">Arch Linux x86_64</a> with <a href="https://kde.org/plasma-desktop/">Plasma Desktop</a></li>
+                        <li>Laptop: <a href="https://asahilinux.org/fedora/">Asahi Fedora Remix</a> with <a href="https://kde.org/plasma-desktop/">Plasma Desktop</a></li>
+                        <li>Mobile: <a href="https://lineageos.org/">LineageOS 21</a></li>
+                    </ul>
+                </li>
                 <li><strong>Coding: </strong>VSCode, WebStorm, IDEA, vim</li>
                 <li><strong>Terminal: </strong>Konsole and Yakuake</li>
             </ul>
@@ -63,11 +66,11 @@ const AboutPage: React.FC = () => {
                 {/*<li><strong>Headphones: </strong>Oppo Enco Buds2</li>*/}
             </ul>
 
-            <h2>Miscellaneous</h2>
-            <ul>
-                <li><a href="#">Resume</a></li>
-                <li><a href="#">Blah</a></li>
-            </ul>
+            {/*<h2>Miscellaneous</h2>*/}
+            {/*<ul>*/}
+            {/*    <li><a href="#">Resume</a></li>*/}
+            {/*    <li><a href="#">Blah</a></li>*/}
+            {/*</ul>*/}
         </>
     );
 };
