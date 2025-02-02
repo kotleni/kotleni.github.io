6d14dfcb42aad531ff3e2549c07e48ddf1f92d79
Viktor Varenik <yavarenikya@gmail.com>
2025-02-02 23:29:06 +0200

    chore: change root to /

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,11 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <App>
             <Routes>
-                <Route path="me-react">
-                    <Route index element={<HomePage/>} />
-                    <Route path="projects" element={<ProjectsPage/>} />
-                    <Route path="me" element={<AboutPage/>} />
-                    <Route path="articles" element={<ArticlesPage/>} />
-                    <Route path="rick"  element={<Rick />} />
-                </Route>
+                <Route index element={<HomePage/>} />
+                <Route path="projects" element={<ProjectsPage/>} />
+                <Route path="me" element={<AboutPage/>} />
+                <Route path="articles" element={<ArticlesPage/>} />
+                <Route path="rick"  element={<Rick />} />
             </Routes>
         </App>
     </BrowserRouter>
