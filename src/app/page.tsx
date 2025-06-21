2a090b47ba14ab003088e23bd005a5571018e743
Viktor Varenik <yavarenikya@gmail.com>
2025-06-21 18:34:30 +0300

    chore: idk a lot of changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,43 @@
 import {Button} from '@/components/ui/button';
-import {NavigationLink} from '@/components/ui/navigation-link';
-
-const NavigationBar = () => {
-  return (
-    <div className="h-18 bg-neutral-900 flex items-center justify-center w-screen">
-      <div className="w-6/10 sm:w-9/10 md:w-8/10 lg:w-6/10 h-full flex items-center p-8 gap-2">
-        <div className="font-bold text-xl">KOTLENI</div>
-        <div className="w-full flex items-center justify-end gap-4">
-          <NavigationLink href="/" title="Home" />
-          <NavigationLink href="/" title="Articles" />
-          <NavigationLink href="/" title="Projects" />
-          <NavigationLink href="/" title="Sandbox" />
-          <NavigationLink href="/" title="About me" />
-        </div>
-      </div>
-    </div>
-  );
-};
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="">
-      <NavigationBar />
-      <div className="flex items-center justify-center p-4">
-        <Button variant="default">About me</Button>
+    <>
+      <div className="h-full w-full flex flex-col bg-jb">
+        <section className="h-full flex-grow flex flex-col items-start justify-center p-24 text-start">
+          <h1 className="text-6xl font-bold mb-4">Viktor Varenik</h1>
+          <p className="text-4xl font-light mb-8 max-w-2xl">
+            Full-Stack Web Developer specializing in building modern
+            applications with Next.js, TypeScript, NestJS, and React.
+          </p>
+          <Button variant="default" size="lg">
+            Contact Me
+          </Button>
+        </section>
+        {/*<section className="p-24">*/}
+        {/*  <h1 className="text-6xl font-bold mb-4">Hobbies</h1>*/}
+        {/*  <p className="text-4xl font-light mb-8 max-w-2xl">*/}
+        {/*    Section about my hobbies and interests.*/}
+        {/*  </p>*/}
+        {/*</section>*/}
+        <section className="flex flex-col gap-4 p-4 text-sm bg-neutral-900 text-neutral-400">
+          <div className="flex flex-row justify-start gap-4">
+            <Link
+              href="https://github.com/kotleni/kotleni.github.io"
+              target="_blank"
+            >
+              Source code
+            </Link>
+            <Link href="/">Servers availability</Link>
+            <Link href="/">Take a hug</Link>
+          </div>
+          <div className="flex flex-row justify-between">
+            <p>Copyright © 2023-2025 Viktor Varenik</p>
+            <p>Made with ❤️ and TypeScript</p>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   );
 }
