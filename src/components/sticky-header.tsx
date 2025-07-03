7bbecfd557c623ad255b2488e6e5b7825ad61361
Viktor Varenik <yavarenikya@gmail.com>
2025-07-03 23:01:59 +0300

    chore: stickyheader no more sticky

--- src/components/sticky-header.tsx
+++ src/components/sticky-header.tsx
@@ -4,7 +4,7 @@ export interface StickyHeaderProps {
 
 export function StickyHeader(props: StickyHeaderProps) {
     return (
-        <div className="sticky top-0 backdrop-blur-sm bg-slate-900/80 z-10 py-2 mt-8 px-8">
+        <div className="z-10 py-2 mt-8 px-8">
             <h2 className="text-sm font-bold uppercase text-slate-200">
                 {props.title}
             </h2>
