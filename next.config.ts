7cb50bd8781522cb4c5be3e3396bd531ab274e3c
Viktor Varenik <yavarenikya@gmail.com>
2025-07-03 13:03:22 +0300

    ci: add stuff for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: 'export',
+    basePath: '/',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
