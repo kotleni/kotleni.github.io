6183d4924daa22512135ab3e23f3a4f30b834f11
Viktor Varenik <yavarenikya@gmail.com>
2025-07-03 13:08:29 +0300

    chore(nextjs): do not provide basePath for root

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/',
+    basePath: '',
     images: {
         unoptimized: true,
     },
