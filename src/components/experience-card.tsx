44f805e502eac4d7e4f9512c746cd0a68cea7b2c
Viktor Varenik <yavarenikya@gmail.com>
2025-07-11 23:51:54 +0300

    fix: wrong cards bg color on hover

--- src/components/experience-card.tsx
+++ src/components/experience-card.tsx
@@ -8,7 +8,7 @@ export interface ExperienceCardProps {
 
 export function ExperienceCard(props: ExperienceCardProps) {
     return (
-        <div className="flex flex-col md:flex-row md:gap-2 md:px-4 md:py-2 md:rounded-md md:transition-all md:hover:bg-slate-800/50 md:group-hover/list:opacity-50 ">
+        <div className="flex flex-col md:flex-row md:gap-2 md:px-4 md:py-2 md:rounded-md md:transition-all md:hover:bg-primary/10 md:group-hover/list:opacity-50 ">
             <span className="md:w-32 flex-shrink-0">
                 <p className="text-xs font-bold uppercase mt-1">
                     {props.startDate} - {props.endDate}
