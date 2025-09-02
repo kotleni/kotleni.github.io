6bc192f1ce556b01680057a94a80837fc2c6c1b2
Viktor Varenik <yavarenikya@gmail.com>
2025-09-02 20:11:42 +0300

    feat(sandbox->physbox): basic manipulating with objects

--- src/app/sandbox/physbox/page.tsx
+++ src/app/sandbox/physbox/page.tsx
@@ -18,6 +18,12 @@ interface Object {
     bounds: Box;
 }
 
+function isPointCollideWithObject(pos: Pos, obj: Object): boolean {
+    const x2 = obj.pos.x + obj.bounds.w;
+    const y2 = obj.pos.y + obj.bounds.h;
+    return pos.x >= obj.pos.x && pos.y >= obj.pos.y && pos.x < x2 && pos.y < y2;
+}
+
 const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -33,6 +39,38 @@ const Canvas = () => {
         if (!ctx) return;
 
         let animationFrameId: number;
+
+        let grabbedObj: Object | undefined = undefined;
+        let grabOffset: Pos = {x: 0, y: 0};
+
+        const mouseDown = (event: MouseEvent) => {
+            objects.forEach(obj => {
+                if (
+                    isPointCollideWithObject(
+                        {x: event.clientX, y: event.clientY},
+                        obj,
+                    )
+                ) {
+                    grabOffset = {
+                        x: Math.floor(event.clientX - obj.pos.x),
+                        y: Math.floor(event.clientY - obj.pos.y),
+                    };
+                    grabbedObj = obj;
+                }
+            });
+        };
+
+        const mouseUp = (event: MouseEvent) => {
+            grabbedObj = undefined;
+        };
+
+        const mouseMove = (event: MouseEvent) => {
+            if (grabbedObj) {
+                const mx = Math.floor(event.clientX);
+                const my = Math.floor(event.clientY);
+                grabbedObj.pos = {x: mx - grabOffset.x, y: my - grabOffset.y};
+            }
+        };
 
         const resizeCanvas = () => {
             canvas.width = window.innerWidth;
@@ -53,11 +91,17 @@ const Canvas = () => {
 
         resizeCanvas();
         window.addEventListener('resize', resizeCanvas);
+        window.addEventListener('mousedown', mouseDown);
+        window.addEventListener('mouseup', mouseUp);
+        window.addEventListener('mousemove', mouseMove);
         draw();
 
         return () => {
             cancelAnimationFrame(animationFrameId);
             window.removeEventListener('resize', resizeCanvas);
+            window.removeEventListener('mousedown', mouseDown);
+            window.removeEventListener('mouseup', mouseUp);
+            window.removeEventListener('mousemove', mouseMove);
         };
     }, []);
 
