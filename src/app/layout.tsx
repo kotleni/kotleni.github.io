83a4c4f4dbb746e4ed4ae3478a11060f10ce2fa9
Viktor Varenik <yavarenikya@gmail.com>
2025-09-06 13:34:52 +0300

    feat: make all root page be in suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ const navLinks: NavLinkInfo[] = [
     {title: 'sandbox', url: '/sandbox'},
 ];
 
-export default function RootLayout({
+function RootLayoutContent({
     children,
 }: Readonly<{
     children: React.ReactNode;
@@ -42,6 +42,85 @@ export default function RootLayout({
     const ref = params.get('ref');
 
     return (
+        <body
+            className={cn(
+                outfit.className,
+                'flex justify-center dark:bg-background',
+            )}
+        >
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <div
+                    className={cn(
+                        'flex flex-col',
+                        isFullsized
+                            ? ''
+                            : 'md:container px-4 sm:px-12 md:px-28 lg:px-60 xl:px-72 2xl:px-99',
+                    )}
+                >
+                    <div
+                        className="w-full bg-accent flex flex-row justify-center items-center gap-2 p-1"
+                        hidden={isFullsized}
+                    >
+                        <p className="text-sm">
+                            I've launched a{' '}
+                            <Link
+                                className="text-primary hover:underline"
+                                href="/sandbox"
+                            >
+                                sandbox
+                            </Link>{' '}
+                            for experimental web projects.
+                        </p>
+                    </div>
+                    <header
+                        className="flex flex-row justify-end p-3 md:p-0"
+                        hidden={isFullsized}
+                    >
+                        <div className="flex flex-row gap-2">
+                            {navLinks.map((link, index) => {
+                                return (
+                                    <NavigationLink
+                                        key={index}
+                                        title={link.title}
+                                        url={link.url}
+                                        isActive={pathName === link.url}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </header>
+                    <div hidden={!isFullsized} className="absolute z-10 p-4">
+                        <Link href={ref ?? ''}>
+                            <SidebarClose
+                                className={cn(
+                                    'cursor-pointer',
+                                    isDarkBg
+                                        ? 'text-neutral-50 hover:text-neutral-400'
+                                        : 'hover:text-accent-foreground',
+                                )}
+                            />
+                        </Link>
+                    </div>
+                    <main className={cn(isFullsized ? '' : 'pb-4')}>
+                        {children}
+                    </main>
+                </div>
+            </ThemeProvider>
+        </body>
+    );
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
         <html lang="en" suppressHydrationWarning>
             <head>
                 <meta
@@ -50,81 +129,9 @@ export default function RootLayout({
                 />
                 <title>kotleni's web</title>
             </head>
-            <body
-                className={cn(
-                    outfit.className,
-                    'flex justify-center dark:bg-background',
-                )}
-            >
-                <Suspense>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="system"
-                        enableSystem
-                        disableTransitionOnChange
-                    >
-                        <div
-                            className={cn(
-                                'flex flex-col',
-                                isFullsized
-                                    ? ''
-                                    : 'md:container px-4 sm:px-12 md:px-28 lg:px-60 xl:px-72 2xl:px-99',
-                            )}
-                        >
-                            <div
-                                className="w-full bg-accent flex flex-row justify-center items-center gap-2 p-1"
-                                hidden={isFullsized}
-                            >
-                                <p className="text-sm">
-                                    I've launched a{' '}
-                                    <Link
-                                        className="text-primary hover:underline"
-                                        href="/sandbox"
-                                    >
-                                        sandbox
-                                    </Link>{' '}
-                                    for experimental web projects.
-                                </p>
-                            </div>
-                            <header
-                                className="flex flex-row justify-end p-3 md:p-0"
-                                hidden={isFullsized}
-                            >
-                                <div className="flex flex-row gap-2">
-                                    {navLinks.map((link, index) => {
-                                        return (
-                                            <NavigationLink
-                                                key={index}
-                                                title={link.title}
-                                                url={link.url}
-                                                isActive={pathName === link.url}
-                                            />
-                                        );
-                                    })}
-                                </div>
-                            </header>
-                            <div
-                                hidden={!isFullsized}
-                                className="absolute z-10 p-4"
-                            >
-                                <Link href={ref ?? ''}>
-                                    <SidebarClose
-                                        className={cn(
-                                            'cursor-pointer',
-                                            isDarkBg
-                                                ? 'text-neutral-50 hover:text-neutral-400'
-                                                : 'hover:text-accent-foreground',
-                                        )}
-                                    />
-                                </Link>
-                            </div>
-                            <main className={cn(isFullsized ? '' : 'pb-4')}>
-                                {children}
-                            </main>
-                        </div>
-                    </ThemeProvider>
-                </Suspense>
-            </body>
+            <Suspense>
+                <RootLayoutContent>{children}</RootLayoutContent>
+            </Suspense>
         </html>
     );
 }
