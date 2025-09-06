e9d33c8baf7eef189b9106d01ef1f95a388c4b61
Viktor Varenik <yavarenikya@gmail.com>
2025-09-06 13:06:27 +0300

    feat: sandboxes page rework UI

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,19 +1,23 @@
 'use client';
 
-import {StyledLink} from '@/components/styled-link';
 import {usePathname} from 'next/navigation';
+import Link from 'next/link';
+import Image, {StaticImageData} from 'next/image';
+import SnakeGameScreenshot from '@/app/sandbox/snakegame/screenshot1.png';
 
 interface SandboxApp {
     name: string;
     description: string;
     url: string;
+    preview: StaticImageData;
 }
 
 const apps: SandboxApp[] = [
     {
         name: 'Snake',
-        description: 'A 2d snake game with a lot of glows.',
+        description: 'A 2D snake game with a lot of glows.',
         url: '/sandbox/snakegame',
+        preview: SnakeGameScreenshot,
     },
 ];
 
@@ -21,16 +25,35 @@ export default function SandboxPage() {
     const pathName = usePathname();
 
     return (
-        <div>
-            {apps.map(app => {
-                return (
-                    <StyledLink
-                        key={app.name}
-                        title={app.name}
-                        href={app.url + '?is_fullsized=1&ref=' + pathName}
-                    />
-                );
-            })}
+        <div className="flex flex-wrap justify-center gap-4 p-4 sm:p-6">
+            {apps.map(app => (
+                <Link
+                    key={`${app.name}`}
+                    href={`${app.url}?is_fullsized=1&ref=${pathName}`}
+                    className="w-full max-w-xs overflow-hidden rounded-md border border-accent/50 bg-accent"
+                >
+                    {/* Image Container */}
+                    <div className="border-b border-accent/50">
+                        <Image
+                            src={app.preview}
+                            alt={`Screenshot of ${app.name}`}
+                            width={320}
+                            height={180}
+                            className="h-30 w-full object-cover"
+                        />
+                    </div>
+
+                    {/* Content Container */}
+                    <div className="p-2">
+                        <h3 className="text-sm font-semibold text-accent-foreground">
+                            {app.name}
+                        </h3>
+                        <p className="mt-1 text-sm text-accent-foreground/80">
+                            {app.description}
+                        </p>
+                    </div>
+                </Link>
+            ))}
         </div>
     );
 }
