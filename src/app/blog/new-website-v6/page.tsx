18e6b3be4830960599aef0b900cee2c030478867
Viktor Varenik <yavarenikya@gmail.com>
2025-11-21 18:30:43 +0200

    feat: basic views count for blog

--- src/app/blog/new-website-v6/page.tsx
+++ src/app/blog/new-website-v6/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import {StyledLink} from '@/components/styled-link';
 import {TextBlock} from '@/components/blog/text-block';
 import {Title} from '@/components/blog/title';
 import {OrderedList} from '@/components/blog/ordered-list';
+import {useEffect, useState} from 'react';
+import {EyeIcon} from 'lucide-react';
+import {Skeleton} from '@/components/ui/skeleton';
 
 const nextjsReasons = [
     {
@@ -21,9 +26,34 @@ const nextjsReasons = [
     },
 ];
 
+interface ViewsReportResponse {
+    count: number;
+}
+
 export default function Post() {
+    const [viewsCount, setViewsCount] = useState(-1);
+
+    const fetchViewsCountAndReport = async () => {
+        const result = await fetch(
+            'https://api.kotleni.pp.ua/views/report?pageId=test',
+            {method: 'POST'},
+        );
+        console.log(result.body);
+        const data = (await result.json()) as ViewsReportResponse;
+        setViewsCount(data.count);
+    };
+
+    useEffect(() => {
+        void fetchViewsCountAndReport();
+    }, []);
+
     return (
         <div className="flex flex-col gap-4 mt-2">
+            <div className="flex flex-row items-center gap-2">
+                <EyeIcon />
+                <p hidden={viewsCount < 0}>{viewsCount}</p>
+                <Skeleton hidden={viewsCount >= 0} className="h-4 w-[25px]" />
+            </div>
             <h1 className="font-bold text-3xl">
                 The sixth iteration of my website.
             </h1>
