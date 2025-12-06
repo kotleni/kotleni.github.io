45418ef79fa9cece7bf1cc017341d9bb93da570c
Viktor Varenik <yavarenikya@gmail.com>
2025-12-06 19:53:56 +0200

    chore(blog): add a new blog post badge

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,11 @@
 import {Title} from '@/components/blog/title';
 import Link from 'next/link';
+import {Badge} from '@/components/ui/badge';
 
 interface Post {
     title: string;
     url: string;
+    isNew: boolean;
     description: string;
 }
 
@@ -11,12 +13,14 @@ const posts: Post[] = [
     {
         title: 'Why i abandoned idea of using Asahi Linux',
         url: '/blog/asahi-linux',
+        isNew: true,
         description:
             "I am a Linux guy. I've used it on my VPS and home lab for years. On my desktop computer, I run Arch Linux exclusively, and I fully enjoy...",
     },
     {
         title: 'The sixth iteration of my website',
         url: '/blog/new-website-v6',
+        isNew: false,
         description:
             "Throughout my life, I've created a bunch of different websites with various designs, but none of them ever felt quite right for my personal page...",
     },
@@ -29,11 +33,14 @@ export default function Blog() {
             {posts.map((post, index) => {
                 return (
                     <div key={index}>
-                        <Link className="hover:underline" href={post.url}>
-                            <p className="font-bold text-primary">
-                                {post.title}
-                            </p>
-                        </Link>
+                        <div className="flex flex-row gap-2">
+                            <Link className="hover:underline" href={post.url}>
+                                <p className="font-bold text-primary">
+                                    {post.title}
+                                </p>
+                            </Link>
+                            <p hidden={!post.isNew} className="font-bold text-destructive">New!</p>
+                        </div>
                         <p>{post.description}...</p>
                     </div>
                 );
