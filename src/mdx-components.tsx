975741c7f4f76f6756b1da4e6664ba9f9b83aed7
Viktor Varenik <yavarenikya@gmail.com>
2025-12-28 22:45:18 +0200

    chore: return back mdx components file

---
+++ src/mdx-components.tsx
@@ -0,0 +1,6 @@
+import type {MDXComponents} from 'mdx/types';
+import {components} from '@/components/markdown-styles';
+
+export function useMDXComponents(): MDXComponents {
+    return components;
+}
