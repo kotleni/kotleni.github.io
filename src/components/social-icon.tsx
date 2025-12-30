ac6154385972219a8dab0a315f9f12e2b97f13a1
Viktor Varenik <yavarenikya@gmail.com>
2025-12-30 22:56:40 +0200

    chore: add aria labels for social links

--- src/components/social-icon.tsx
+++ src/components/social-icon.tsx
@@ -1,12 +1,13 @@
 interface SocialIconProps {
     Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
     url: string;
+    label: string;
 }
 
-export function SocialIcon({Icon, url}: SocialIconProps) {
+export function SocialIcon({Icon, url, label}: SocialIconProps) {
     return (
         <a href={url} target="_blank">
-            <Icon className="size-4 cursor-pointer hover:text-primary" />
+            <Icon className="size-4 cursor-pointer hover:text-primary" aria-label={label} />
         </a>
     );
 }
