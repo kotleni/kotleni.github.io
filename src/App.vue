ecf36864b526fe5aec212fe8f8f256a0d2e41b84
Viktor Varenik <yavarenikya@gmail.com>
2026-03-10 16:39:10 +0200

    feat: transition animations

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,24 @@ useDark({
                 <RouterLink class="link" to="/blog">Blog</RouterLink>
             </nav>
         </div>
-        <RouterView />
+        <RouterView v-slot="{Component}">
+            <Transition name="fade" mode="out-in">
+                <component :is="Component" />
+            </Transition>
+        </RouterView>
     </main>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
 .topbar {
     width: 100%;
     padding: 8px;
